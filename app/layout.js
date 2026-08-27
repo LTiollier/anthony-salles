7f6f62d7fe2633b40fa0af40fca7fd5523ef927e
ltiollier <ltiollier30@gmail.com>
2026-08-27 16:25:22 +0200

    ⏪️ Revert Traditional Chinese Medicine page

--- app/layout.js
+++ app/layout.js
@@ -10,13 +10,12 @@ export const metadata = {
     template: "%s | Anthony Salles",
   },
   description:
-    "Expertise en kinésithérapie, chaînes musculaires GDS, médecine chinoise et thérapie viscérale à Lyon 7. Accompagnement holistique et personnalisé.",
+    "Expertise en kinésithérapie, chaînes musculaires GDS et thérapie viscérale à Lyon 7. Accompagnement holistique et personnalisé.",
   keywords: [
     "Kinésithérapeute",
     "Lyon",
     "Lyon 7",
     "Chaînes musculaires GDS",
-    "Médecine Chinoise",
     "Thérapie Viscérale",
     "Santé",
     "Bien-être",
@@ -38,7 +37,7 @@ export const metadata = {
   openGraph: {
     title: "Anthony Salles - Kinésithérapeute à Lyon 7",
     description:
-      "Expertise en kinésithérapie, chaînes musculaires GDS, médecine chinoise et thérapie viscérale. Accompagnement holistique et personnalisé.",
+      "Expertise en kinésithérapie, chaînes musculaires GDS et thérapie viscérale. Accompagnement holistique et personnalisé.",
     url: "https://anthony-salles.vercel.app",
     siteName: "Anthony Salles",
     locale: "fr_FR",
@@ -48,7 +47,7 @@ export const metadata = {
     card: "summary_large_image",
     title: "Anthony Salles - Kinésithérapeute",
     description:
-      "Kinésithérapie, Chaînes Musculaires GDS, Médecine Chinoise & Thérapie Viscérale à Lyon 7.",
+      "Kinésithérapie, Chaînes Musculaires GDS & Thérapie Viscérale à Lyon 7.",
   },
   robots: {
     index: false,
